a020282899c1b4b077fae0c95ed14c7486bf1d0b
BINTANG RIZQI PASHA <bintangrp45@gmail.com>
2024-12-13 01:36:21 +0700

    feat: favicon web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Business Buddy</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon-32x32.png') }}">
     @vite(['./resources/css/app.css', './resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
